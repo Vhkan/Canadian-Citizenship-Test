805f4a0831bc5d154aa191b0432bb5f88c22e8bf
Griego86 <dimi1086@hotmail.com>
2024-04-05 16:43:06 -0700

    Filled up seeds 3 & 4

--- backend/db/seeds/03_questions.sql
+++ backend/db/seeds/03_questions.sql
@@ -1,34 +1,34 @@
 INSERT INTO questions (category_id, question_text, difficulty)
 VALUES
-('Who are the Métis?',3),
-('Which party becomes the Official Opposition?',2),
-('When is Remembrance Day celebrated?',1),
-('What is the highest military honour a Canadian can receive?',3),
-('What is a Francophone?',1),
-('From where does the name “Canada” come from?',,2),
-('Give an example of where English and French have equal status in Canada.',3),
-('How does a bill become a law?',3),
-('Name a responsibility of citizenship.',,1),
-('Name a right protected by the Canadian Charter of Rights and Freedoms',2)
-('What is a territory of Northern Canada and its capital city?',1)
-('What are the three levels of government in Canada?',2)
-('What are the three main groups of Aboriginal peoples?',2)
-('What are the three parts of Parliament?',3)
-('What are the two official languages of Canada?',1)
-('What did the Canadian Pacific Railway symbolize?',3)
-('What did the Fathers of Confederation do?',3)
-('What do you call the Sovereign’s representative in the provinces?',2)
-('What does the “right to a secret ballot” mean?',2)
-('What is Canada’s system of government called?',3)
-('What is the “head tax”?',2)
-('What is the highest court in Canada?',1)
-('What part of the Constitution legally protects the basic rights and freedoms of all Canadians?',2)
-('What song is Canada’s national anthem?',1)
-('What was the “Underground Railroad”?',3)
-('What year did Newfoundland and Labrador join Canada?',2)
-('When must federal elections be held?',2)
-('Where do most French-speaking Canadians live?',1)
-('Which animal is an official symbol of Canada?',2)
-('Which of the following is NOT a feature of Canada’s system of government?',2)
+(1, 'Who are the Métis?', 3),
+(2, 'Which party becomes the Official Opposition?',2),
+(1, 'When is Remembrance Day celebrated?',1),
+(2, 'What is the highest military honour a Canadian can receive?',3),
+(3, 'What is a Francophone?',1),
+(1, 'From where does the name “Canada” come from?',,2),
+(3, 'Give an example of where English and French have equal status in Canada.',3),
+(2, 'How does a bill become a law?',3),
+(5, 'Name a responsibility of citizenship.',1),
+(5, 'Name a right protected by the Canadian Charter of Rights and Freedoms',2),
+(4, 'What is a territory of Northern Canada and its capital city?',1),
+(2, 'What are the three levels of government in Canada?',2),
+(1, 'What are the three main groups of Aboriginal peoples?',2),
+(2, 'What are the three parts of Parliament?',3),
+(3, 'What are the two official languages of Canada?',1),
+(1, 'What did the Canadian Pacific Railway symbolize?',3),
+(1, 'What did the Fathers of Confederation do?',3),
+(2, 'What do you call the Sovereign’s representative in the provinces?',2),
+(5, 'What does the “right to a secret ballot” mean?',2),
+(2, 'What is Canada’s system of government called?',3),
+(1, 'What was the “head tax”?',2),
+(2, 'What is the highest court in Canada?',1),
+(2, 'What part of the Constitution legally protects the basic rights and freedoms of all Canadians?',2),
+(1, 'What song is Canada’s national anthem?',1),
+(1, 'What was the “Underground Railroad”?',3),
+(1, 'What year did Newfoundland and Labrador join Canada?',2),
+(2, 'When must federal elections be held?',2),
+(3, 'Where do most French-speaking Canadians live?',1),
+(1, 'Which animal is an official symbol of Canada?',2),
+(6, 'Which of the following is NOT a feature of Canada’s system of government?',2);
 
 -- Level of difficulty: 3 hard, 2 medium, 1 easy

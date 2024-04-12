5f3901cd9c1ca83b11d930c383bfa69e3e00c499
Vhkan <vhkan@yahoo.com>
2024-04-12 15:09:43 -0600

    add user's test progress tracking logic

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -4,12 +4,17 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const Test = () => {
-
+  //Setttign q/a to [] to keep track of the q/a progress
   const [questionAnswer, setQuestionAnswer] = useState(null);
   const [loading, setLoading] = useState(true);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [result, setResult] = useState(null);
-  const [remainingQuestions, setRemainingQuestions] = useState(20);
+  // const [remainingQuestions, setRemainingQuestions] = useState(20);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  //Keep track of incorrect questions id's to pull out later from a db on a score/result component
+  const [incorrectAnswers, setIncorrectAnswers] = useState([]);
+  const [answeredQuestions, setAnsweredQuestions] = useState(0);
+
 
   //Fetching questions from the server
   useEffect(() => {
@@ -17,6 +22,11 @@ const Test = () => {
   }, []);
 
   const fetchQuestions = () => {
+    if(answeredQuestions >= 20) {
+      setLoading(false);
+      alert("Test completed! Your score will be here soon!");
+      return; //Stop fetching when 20 questions are answered
+    }
     axios.get("http://localhost:9000/test")
       .then(response => {
         if (!response.data) {
@@ -38,19 +48,32 @@ const Test = () => {
     setSelectedAnswer(e.target.value)
   };
 
+
   //Handling user's answer submit
   const handleSubmit = () => {
+    //Prevent submitting without a selected answer
+    if (!selectedAnswer) return;
+
     //Checking if the answer was chosen is correct
     const correctAnswer = questionAnswer.answers.find(answer => answer.is_correct).answer_id.toString();
     console.log("Correct answer is:", correctAnswer);
     if(selectedAnswer === correctAnswer) {
       setResult("Correct answer!");
-      fetchQuestions();
+      setCorrectAnswers(prev => prev + 1)
+      // fetchQuestions();
     } else {
       setResult("Incorrect answer!");
+      setIncorrectAnswers(prev => [...prev, questionAnswer.question_id])
     }
+    setAnsweredQuestions(prev => prev + 1);
+    //Reset selection for the next question
+    setSelectedAnswer(null);
+    setTimeout(() => {
+      setResult(null);
+      fetchQuestions();
+    }, 2000);
     //Decrease question count
-   setRemainingQuestions(prevCount => prevCount - 1); 
+  //  setRemainingQuestions(prevCount => prevCount - 1); 
   };
   
   //In case the no data is fetched from the DB
@@ -58,11 +81,23 @@ const Test = () => {
     return <p>Loading...</p>;
   }
 
+  //For testing purposes: show the test score. Later will be resirect to a new (score/results) page
+  if(answeredQuestions >= 20) {
+      return (
+        <div>
+          <h1>Test Completed!</h1>
+          <p> Correct Answers: {correctAnswers} </p>
+          {/* will be the array of id's length */}
+          <p> Incorrect Answers: {incorrectAnswers.length}</p> 
+        </div>
+      )
+  }
+
 
   return (
     <div>
         <div>
-           <h3>Question {21 - remainingQuestions} of 20</h3>
+           <h3>Question {answeredQuestions + 1} of 20</h3>
            <p>{questionAnswer.question_text}</p>
            <h3>Answers:</h3>
 
@@ -82,9 +117,10 @@ const Test = () => {
           <div>
           <Button variant="outline-success" onClick={handleSubmit}>Submit Answer</Button> 
           <Button variant="outline-warning">Skip Question</Button>
+
+          {/* make the result dissappear in 3 seconds in the future for better UI */}
           {result && <p>{result}</p>}
           </div>
-          
         </div>  
     </div>
   );

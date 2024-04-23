d5b3ae3047dac418f665fc29e64343464b695900
Vhkan <vhkan@yahoo.com>
2024-04-22 23:18:59 -0600

    add link highlight

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -34,6 +34,10 @@ export default function TopNavBar() {
     navigate('/');
   };
 
+  //highlight the link the user is on now
+  const activeLinkClass = (path) => {
+    return location.pathname === path ? 'active' : '';
+  };
 
   return (
     <div className='top-nav-bar'>
@@ -44,10 +48,10 @@ export default function TopNavBar() {
         </a>
       </div>      
       <div className='top-nav-links'>
-        <a href="/" className='nav-link'>Home</a>
-        <a href="/prepare" className='nav-link'>Prepare</a>
-        <a href="/about"className='nav-link'>About</a>
-        <a href="/test"className='nav-link'>Test</a>
+        <a href="/" className={`nav-link ${activeLinkClass('/')}`}>Home</a>
+        <a href="/prepare" className={`nav-link ${activeLinkClass('/prepare')}`}>Prepare</a>
+        <a href="/about" className={`nav-link ${activeLinkClass('/about')}`}>About</a>
+        <a href="/test" className={`nav-link ${activeLinkClass('/test')}`}>Test</a>
       </div>
       {isLoggedIn ? (
         <div>

c29bd8cdb941093303f9544b30319be4fb23ab9c
Vhkan <vhkan@yahoo.com>
2024-04-22 19:00:14 -0600

    edit about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       for the Citizenship Test in order to become citizens of Canada. We were looking for a helpful and useful application to get ready 
       for the test. Finding one at that time was not an easy task. So, the decision was made to develop this application for future newcomers 
       to Canada dreaming of becoming its citizens, like us the other day. Once it was a dream to come to Canada and become its citizen. Later, 
-      it was a dream to learn computer programming. Dream, as dreams come true!</p>
+      it was a dream to become a software developer. Dream, as dreams come true!</p>
         <p className="about">This project was made from scratch as part of the Lighthouse Labs Bootcamp Educational Program.</p>
     </div>
   )
